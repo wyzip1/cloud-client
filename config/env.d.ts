cfea1dc100bdc4426dd11c6977e082822c1b2a4f
wyzip <2362086452@qq.com>
2025-05-20 11:48:49 +0800

    类型调整

--- config/env.d.ts
+++ config/env.d.ts
@@ -175,6 +175,39 @@ interface ShowShareMenuOptions {
   menus?:	Array<'shareAppMessage' | 'shareTimeline'>	// 否	['shareAppMessage'、'shareTimeline']	需要显示的转发按钮名称列表	v2.149.4
 }
 
+interface SystemEnv {
+  USER_DATA_PATH: string
+}
+
+interface WriteFileOptions {
+  filePath: string
+  data: string
+  encoding: "base64"
+}
+
+interface FileSystemManager {
+  writeFile: (options: WriteFileOptions) => Promise<any>
+  close: () => void
+}
+
+interface OpenDocumentOptions {
+  filePath: string
+  showMenu: boolean
+}
+
+interface ChooseMessageFileOptions {
+  count: number,
+  type: "file",
+  extension: string[],
+}
+
+interface ChooseMessageFileResult {
+  tempFiles: Array<{
+    size: number;
+    path: string
+  }>
+}
+
 declare namespace yz {
   function request(config: RequestConfig): Promise<RequestResponse>;
 
@@ -244,6 +277,23 @@ declare namespace yz {
 
   function showShareMenu(options?: ShowShareMenuOptions): Promise<void>
 
+  function getSystemEnv(): Promise<SystemEnv>
+
+  function getFileSystemManager(): Promise<FileSystemManager>
+
+  function openDocument(options: OpenDocumentOptions): Promise<any>
+
+  function chooseMessageFile(options: ChooseMessageFileOptions): Promise<ChooseMessageFileResult>
+
+  function getImageInfo(options: {src: string}): Promise<{
+    errMsg: string;
+    height: number
+    orientation: string;
+    path: string;
+    type: string;
+    width: number
+  }>
+
   interface Logger {
     info(options: LoggerOptions): void
     warn(options: LoggerOptions): void
@@ -261,53 +311,3 @@ declare namespace yz {
 
   export const event: Event
 }
-
-declare function extendPage (config: {
-  created(this: { yz: YzPageApi }, options: object): void;
-  onShow(this: { yz: YzPageApi }): void;
-  onHide(this: { yz: YzPageApi }): void;
-  beforeMount(this: { yz: YzPageApi },options: Record<string, any>): void;
-  mounted(this: { yz: YzPageApi }): void;
-  dataReady(this: { yz: YzPageApi }): void;
-  destroyed(this: { yz: YzPageApi }): void;
-  methods: Record<string, (this: { yz: YzPageApi }, ...args: any[]) => any>;
-}): any
-
-declare function extendApp(config: {
-   // 监听应用初始化生命周期
-   onLaunch(this: { yz: YzPageApi }, options: object): void;
-   // 监听应用切换至前台生命周期
-   onShow(this: { yz: YzPageApi }, options: object): void;
-   // 监听应用切换至后台生命周期
-   onHide(this: { yz: YzPageApi }): void;
-   // 应用级别自定义方法（部分原生应用钩子也需放置到此处，以保证正确的上下文）
-   methods: Record<string, (this: { yz: YzPageApi }, ...args: any[]) => any>;
-}): any
-
-declare function createPage(config: {
-  created(this: { yz: YzPageApi }, options: object): void;
-  onShow(this: { yz: YzPageApi }): void;
-  onHide(this: { yz: YzPageApi }): void;
-  beforeMount(this: { yz: YzPageApi }, options: Record<string, any>): void;
-  mounted(this: { yz: YzPageApi }): void;
-  destroyed(this: { yz: YzPageApi }): void;
-  methods: Record<string, Function>;
-  // 小程序分享配置项。H5 不支持
-  onShareAppMessage(this: { yz: YzPageApi }, options: { form: 'button' | 'menu', target?: "button", webViewUrl?: string }): {
-    title?: string;
-    path?: string;
-    imageUrl?: string;
-    promise: Promise<{
-      title?: string;
-      path?: string;
-      imageUrl?: string
-    }>
-  }
-  // 小程序下拉刷新事件。H5 不支持
-  onPullDownRefresh(this: { yz: YzPageApi }): void;
-  // 小程序上拉触底事件。H5 不支持
-  onReachBottom(this: { yz: YzPageApi }): void
-  // 小程序页面滚动事件。H5 不支持
-  onPageScroll(this: { yz: YzPageApi }, ...args: any[]): void
-  render: (h: any) => any
-})
--- config/vue.d.ts
+++ config/vue.d.ts
@@ -34,10 +34,63 @@ declare global {
     app: {
       getApi: () => ReturnType<typeof registerRequestApi>
       customRequest: CustomRequest
+      getCustomUrl: (path: string) => string
+      isDev: () => boolean
     }
   
-    $getPageQuery: () => Record<string, any>
+    getPageQuery: () => Record<string, any>
   }
+
+
+  function extendPage(config: {
+    created(this: { yz: YzPageApi }, options: object): void;
+    onShow(this: { yz: YzPageApi }): void;
+    onHide(this: { yz: YzPageApi }): void;
+    beforeMount(this: { yz: YzPageApi }, options: Record<string, any>): void;
+    mounted(this: { yz: YzPageApi }): void;
+    dataReady(this: { yz: YzPageApi }): void;
+    destroyed(this: { yz: YzPageApi }): void;
+    methods: Record<string, (this: { yz: YzPageApi }, ...args: any[]) => any>;
+  }): any
+
+  function extendApp(config: {
+    // 监听应用初始化生命周期
+    onLaunch(this: { yz: YzPageApi }, options: object): void;
+    // 监听应用切换至前台生命周期
+    onShow(this: { yz: YzPageApi }, options: object): void;
+    // 监听应用切换至后台生命周期
+    onHide(this: { yz: YzPageApi }): void;
+    // 应用级别自定义方法（部分原生应用钩子也需放置到此处，以保证正确的上下文）
+    methods: Record<string, (this: { yz: YzPageApi }, ...args: any[]) => any>;
+  }): any
+
+  function createPage(config: {
+    created(this: { yz: YzPageApi }, options: object): void;
+    onShow(this: { yz: YzPageApi }): void;
+    onHide(this: { yz: YzPageApi }): void;
+    beforeMount(this: { yz: YzPageApi }, options: Record<string, any>): void;
+    mounted(this: { yz: YzPageApi }): void;
+    destroyed(this: { yz: YzPageApi }): void;
+    methods: Record<string, Function>;
+    // 小程序分享配置项。H5 不支持
+    onShareAppMessage(this: { yz: YzPageApi }, options: { form: 'button' | 'menu', target?: "button", webViewUrl?: string }): {
+      title?: string;
+      path?: string;
+      imageUrl?: string;
+      promise: Promise<{
+        title?: string;
+        path?: string;
+        imageUrl?: string
+      }>
+    }
+    // 小程序下拉刷新事件。H5 不支持
+    onPullDownRefresh(this: { yz: YzPageApi }): void;
+    // 小程序上拉触底事件。H5 不支持
+    onReachBottom(this: { yz: YzPageApi }): void
+    // 小程序页面滚动事件。H5 不支持
+    onPageScroll(this: { yz: YzPageApi }, ...args: any[]): void
+    render: (h: any) => any
+  })
 }
 
 // Vue has the constructor type in types/vue.d.ts
