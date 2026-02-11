2504a273ddc6e62ccb192d386a710e735de6b372
略。 <oncwnuCmzY4Z4L9bDh4p_wvLdYU8@git.weixin.qq.com>
2026-02-11 18:50:59 +0800

    feat(api): 添加图片上传功能并移除测试请求
    添加uploadImg方法用于图片上传，支持自定义表单数据和开发环境判断
    移除不再使用的testRequest方法

--- src/api/index.js
+++ src/api/index.js
@@ -1,26 +1,34 @@
 /**
  *
  * @param {import("../../config/vue").CustomRequest} request
+ * @param {yz} yz
+ * @param {() => boolean} isDev
  * @returns
  */
-export default function registerRequestApi(request) {
-  /**
-   * 
-   * @param {object} data
-   * @param {number} data.pageNum
-   * @param {number} data.pageSize
-   * @param {string} data.yzOpenId
-   * @returns {Promise<RequestResponse>}
-   */
-  const testRequest = (data) => {
-    return request({
-      url: "/test",
-      method: "GET",
-      data,
-    });
-  }
+export default function registerRequestApi(request, yz, isDev) {
+  // 上传图片
+  const uploadImg = (filePath, formData = {}) => {
+    return yz
+      .uploadFile({
+        path: "/api/v1/client/material/image/upload",
+        filePath,
+        formData,
+        fileType: "image",
+        dev: isDev(),
+      })
+      .then((res) => {
+        try {
+          res.data = JSON.parse(res.data || null);
+          if (res.code !== 200 || (res.data || {}).code !== 200)
+            return Promise.reject(res);
+          return res.data;
+        } catch (error) {
+          return res;
+        }
+      });
+  };
 
   return {
-    testRequest
-  }
+    uploadImg,
+  };
 }
